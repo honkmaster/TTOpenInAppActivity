0903bb5edea36c7f12667ccf7188602b5643dffa
Guy Shaviv <guy@tivstudio.com>
2013-08-23 14:17:21 +0300

    weak delegate instead of unsafe

--- TTOpenInAppActivity/TTOpenInAppActivity.m
+++ TTOpenInAppActivity/TTOpenInAppActivity.m
@@ -109,7 +109,7 @@
 - (NSString *)UTIForURL:(NSURL *)url
 {
     CFStringRef UTI = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, (__bridge CFStringRef)url.pathExtension, NULL);
-    return (__bridge NSString *)UTI;
+    return (NSString *)CFBridgingRelease(UTI) ;
 }
 
 - (void)openDocumentInteractionController
